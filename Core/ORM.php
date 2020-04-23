d3a6ec3977991c022ba29a02a60f906c5575685f
Koumba Keita <keitakoumba97@gmail.com>
2020-04-23 16:57:44 +0200

    23 avril

--- Core/ORM.php
+++ Core/ORM.php
@@ -3,18 +3,22 @@ class ORM{
 
     public function create($table, $fields){
         //retourne un id   
+        "INSERT into $table() VALUES()"; 
     }
 
     public function read($table, $id){
         //retourne un tableau associatif de l' enregistrement
+        "SELECT * FROM $table WHERE id=$id"; 
     } 
    
     public function update($table, $id, $fields){
         //retourne un booleen  
+        "UPDATE $table SET $fields WHERE id=$id";
     } 
 
     public function delete($table, $id){
         //retourne un booleen
+        "DELETE FROM $table WHERE id=$id";
     } 
 
     public function find($table, $params=array('WHERE'=> '1', 'ORDER BY' => 'id ASC', 'LIMIT ' => '') ) {
